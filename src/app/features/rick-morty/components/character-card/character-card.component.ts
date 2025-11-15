078386bdc5202cc4429e59cee953339ffb635140
emaestro <eduard.maestro@elma.care>
2025-11-15 14:25:45 +0100

    Improved layout structure

--- src/app/features/rick-morty/components/character-card/character-card.component.ts
+++ src/app/features/rick-morty/components/character-card/character-card.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RMCharacter } from '../../rick-morty.model';
+import { RMCharacter, CharacterStatus, STATUS_COLOR } from '../../rick-morty.model';
 
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './character-card.component.html',
   styleUrls: ['./character-card.component.scss'],
 })
 export class CharacterCardComponent {
   @Input() character!: RMCharacter;
+
+  CharacterStatus = CharacterStatus; // para usar en el template
+  STATUS_COLOR = STATUS_COLOR;
 }
--- src/app/features/rick-morty/rick-morty.model.ts
+++ src/app/features/rick-morty/rick-morty.model.ts
@@ -1,24 +1,28 @@
-export interface RMCharacterLocation {
-  name: string;
-  url: string;
-}
-
-export interface RMCharacterOrigin {
-  name: string;
-  url: string;
-}
-
-export interface RMCharacter {
+export type RMCharacter = {
   id: number;
   name: string;
-  status: 'Alive' | 'Dead' | 'unknown';
+  status: CharacterStatus;
   species: string;
   type: string;
-  gender: 'Female' | 'Male' | 'Genderless' | 'unknown';
-  origin: RMCharacterOrigin;
-  location: RMCharacterLocation;
+  gender: string;
+  origin: { name: string; url: string };
+  location: { name: string; url: string };
   image: string;
   episode: string[];
   url: string;
   created: string;
+};
+
+/* Enum para el estado del personaje */
+export enum CharacterStatus {
+  Alive = 'Alive',
+  Dead = 'Dead',
+  Unknown = 'unknown',
 }
+
+/* Función de ayuda para obtener color según status */
+export const STATUS_COLOR: Record<CharacterStatus, string> = {
+  [CharacterStatus.Alive]: '#55cc44',
+  [CharacterStatus.Dead]: '#d63d2e',
+  [CharacterStatus.Unknown]: '#9e9e9e',
+};
